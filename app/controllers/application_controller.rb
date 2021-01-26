085d3dbca6bbf4b07da212744dff819c74abcbef
Le Manh An <vipcafe1994@gmail.com>
2021-01-26 09:41:42 +0700

    update I18N

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale)
+    if I18n.available_locales.include?(locale) == true then locale
+    else
+      I18n.default_locale
+    end
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :posts
   get 'static_pages/home'
   get 'static_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    resources :microposts
+    resources :users
+  end
 end
+
