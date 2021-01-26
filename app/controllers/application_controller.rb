1a13aa644c8729795afe84bec0189daac6d6026a
Le Manh An <vipcafe1994@gmail.com>
2021-01-26 10:30:00 +0700

    update I18N

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-
+   before_action :set_locale
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
     locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale)
-    if I18n.available_locales.include?(locale) == true then locale
-    else
-      I18n.default_locale
-    end
+    check = I18n.available_locales.include?(locale)
+    return I18n.locale = locale if check == true
+    return I18n.locale = I18n.default_locale if check == false
   end
 
   def default_url_options
